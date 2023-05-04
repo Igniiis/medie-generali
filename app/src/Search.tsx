06008c64d943a4203cb711e112b41306e001081d
MaxenceM <66200976+Igniiis@users.noreply.github.com>
2023-05-04 23:10:28 +0200

    first deploy

--- app/src/Search.tsx
+++ app/src/Search.tsx
@@ -43,10 +43,6 @@ const Search = () => {
 
       <div className="notes">
         <p>
-          <a href="/?page=calculator&groups=Math%C3%A9matiques:Probabilit%C3%A9s%20Avanc%C3%A9es$1|Recherche%20Op%C3%A9rationnelle$1~2,Informatique%201:Conception%20de%20Syst%C3%A8me%20d%27Information$1|Interface%20Homme%20Machine$1~3,Informatique%202:Compilation$1|Complexit%C3%A9%20Algorithmique$1~3,Stage:Stage$1~1,Transversale:Anglais$1|CHOI%20LV2$1|Technique%20de%20Communication$1~1">link</a>
-            (to delete)
-          <br/>
-          <br/>
           Note : you can use your copied link directly onto your browser to open the same window.
         </p>
       </div>
