f28f7003c60f1eb462d0f19f4b34e6e3bbef2c38
MaxenceM <66200976+Igniiis@users.noreply.github.com>
2023-05-04 23:12:11 +0200

    Updates

---
+++ precache-manifest.2dc14da4a838e4549f4c2a3788629c72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "885c1ca0972c9a4bc6af",
+    "url": "/medie-generali/static/css/main.dd6ca1f6.chunk.css"
+  },
+  {
+    "revision": "885c1ca0972c9a4bc6af",
+    "url": "/medie-generali/static/js/main.885c1ca0.chunk.js"
+  },
+  {
+    "revision": "8a1229037873ab4e6605",
+    "url": "/medie-generali/static/js/1.8a122903.chunk.js"
+  },
+  {
+    "revision": "834ec5985aec2a52ba85",
+    "url": "/medie-generali/static/js/2.834ec598.chunk.js"
+  },
+  {
+    "revision": "69ee91d5dca1e539b443",
+    "url": "/medie-generali/static/js/runtime~main.69ee91d5.js"
+  },
+  {
+    "revision": "5fa16f0ea769f6cea08b94321c457347",
+    "url": "/medie-generali/index.html"
+  }
+];
